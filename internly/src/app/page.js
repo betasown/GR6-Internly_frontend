c4e877cf1eeda942bce7f840d8bbe1adb863767b
Robin Noiret <119452365+RobinNoiret@users.noreply.github.com>
2025-02-10 11:56:25 +0100

    feat: ajout de styles et de nouvelles classes pour la page d'accueil

--- internly/src/app/page.js
+++ internly/src/app/page.js
@@ -3,33 +3,35 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <img src="/Assets/HP-Background.png" alt="LogoInternly"/>
+      <img className="HP-Background" src="/Assets/HP-Background.png"/>
 
       {
       /*_____________________________________________________________________*/
       /*                            STRAT - A PROPOS                         */
       /*_____________________________________________________________________*/
       }
+      <div className="HP_STRAT1-APropos">
 
-      <p className="Paragraphe">A prospos de</p>
-      <h1 className="Titre">Internly</h1>
+        <p className="Paragraphe">A prospos de</p>
+        <h1 className="Titre">Internly</h1>
       
       
 
-      <div className="HP-Statistiques">
-        <div className="trapezoid">
-          <h1 className="Titre">+2K</h1>
-          <p className="Paragraphe">Etudiants</p>
-        </div>
+        <div className="HP-Statistiques">
+          <div className="HP-ContentTrapez_v1">
+            <h1 className="Titre">+2K</h1>
+            <p className="Paragraphe">Etudiants</p>
+          </div>
 
-        <div className="trapezoid">
-          <h1 className="Titre">+80K</h1>
-          <p className="Paragraphe">Offres</p>
-        </div>
+          <div className="HP-ContentTrapez_v2">
+            <h1 className="Titre">+80K</h1>
+            <p className="Paragraphe">Offres</p>
+          </div>
 
-        <div className="trapezoid">
-          <h1 className="Titre">+390</h1>
-          <p className="Paragraphe">Entreprises</p>
+          <div className="HP-ContentTrapez_v1">
+            <h1 className="Titre">+390</h1>
+            <p className="Paragraphe">Entreprises</p>
+          </div>
         </div>
       </div>
 
@@ -44,19 +46,19 @@ export default function Home() {
       <h1 className="Titre">Domaines</h1>
       
       <div className="HP-Domaines">
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre">IT</h1>
         </div>
 
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre">BTP</h1>
         </div>
 
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre">Cyber</h1>
         </div>
 
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre"></h1>
         </div>  
       </div>
@@ -74,23 +76,20 @@ export default function Home() {
       <h1 className="Titre">Confiance</h1>
       
       <div className="HP-Entreprises">
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre">Apple</h1>
         </div>
 
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre">Nvidia</h1>
         </div>
 
-        <div className="trapezoid">
+        <div className="HP-ContentTrapez">
           <h1 className="Titre">Google</h1>
         </div>
       </div>
 
       <a href="/" title="Découvrir les entreprises">Afficher toutes les entreprises</a>
-
-
-
 
 
     </div>
