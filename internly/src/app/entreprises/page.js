dc8a4fcf0f11a60d11ea34101111201963950f41
betasown <louis.drouhin@gmail.com>
2025-02-11 15:48:33 +0100

    ajout de la page entreprise

--- internly/src/app/entreprises/page.js
+++ internly/src/app/entreprises/page.js
@@ -1,8 +1,87 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Entreprises</h1>
-      <p>Page Entreprises</p>
-    </div>
-  );
+export default function EntreprisesPage() {
+    return (
+        <div>
+        <div className="white-entreprises">
+            
+            <div className="title-container"> 
+                <p className="Paragraphe">Découvrir les entreprises</p>
+                <h1 className="Titre">Partenaires</h1>
+            </div>
+
+            <div className="entreprises-page no-gap"> {/* Ajout de la classe "no-gap" */}
+
+                <div className="Container">
+                    <div className="HP-TrapezContainer-entreprise ">
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v3"></div>
+                            <div className="HP-ContentTrapezSmallv3"><img src="/Assets/apple-logo.png" alt="Apple" /></div>
+                        </div>
+
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v4"></div>
+                            <div className="HP-ContentTrapezSmallv4"><img src="/Assets/nvidia-logo.png" alt="Nvidia" /></div>
+                        </div>
+
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v3"></div>
+                            <div className="HP-ContentTrapezSmallv3"><img src="/Assets/google-logo.png" alt="Google" /></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Container">
+                    <div className="HP-TrapezContainer-entreprise ">
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v4"></div>
+                            <div className="HP-ContentTrapezSmallv4"><img src="/Assets/apple-logo.png" alt="Apple" /></div>
+                        </div>
+
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v3"></div>
+                            <div className="HP-ContentTrapezSmallv3"><img src="/Assets/nvidia-logo.png" alt="Nvidia" /></div>
+                        </div>
+
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v4"></div>
+                            <div className="HP-ContentTrapezSmallv4"><img src="/Assets/google-logo.png" alt="Google" /></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Container">
+                    <div className="HP-TrapezContainer-entreprise ">
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v3"></div>
+                            <div className="HP-ContentTrapezSmallv3"><img src="/Assets/apple-logo.png" alt="Apple" /></div>
+                        </div>
+
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v4"></div>
+                            <div className="HP-ContentTrapezSmallv4"><img src="/Assets/nvidia-logo.png" alt="Nvidia" /></div>
+                        </div>
+
+                        <div className="Trapez">
+                            <div className="HP-ContentTrapez_v3"></div>
+                            <div className="HP-ContentTrapezSmallv3"><img src="/Assets/google-logo.png" alt="Google" /></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+
+            <div className="HP_STRAT1-APropos">
+                <div className="content-container">
+                    <div className="text-content">
+                        <h1 className="Titre">Lorem Ipsum</h1>
+                        <p className="Paragraphe">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed rutrum justo, in tristique nulla. In risus velit, suscipit nec risus eu, vestibulum vestibulum erat. Duis in placerat ligula, interdum ultricies turpis. Quisque gravida mi eu diam mattis dictum. Donec euismod massa sem, quis consectetur purus gravida non. Sed egestas hendrerit enim, ultrices maximus velit blandit eget. Morbi sit amet tellus a elit pretium auctor molestie ac velit. Morbi dolor purus, eleifend vitae gravida non, mattis in risus. Proin non enim quis orci laoreet faucibus.
+                        </p>
+                    </div>
+                    <img src="/Assets/google-logo.png" alt="Google" />
+                </div>
+            </div>
+
+        </div>
+    );
 }
