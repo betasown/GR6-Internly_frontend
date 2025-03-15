31b45cf7818dd677e7ee7cc2d5cc66ce52c1e863
Robin <robin.noiret2@gmail.com>
2025-03-15 17:37:44 +0100

    feat: implémentation de la récupération des offres
    feat: mise en place d'une pagination similaire aux entreprises

--- internly/src/app/offres/page.js
+++ internly/src/app/offres/page.js
@@ -1,132 +1,49 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
-    const offres = [
-        {
-            id: 1,
-            entreprise: "Google",
-            titre: "Software Engineer",
-            description: "Develop and maintain web applications.",
-            competences: ["JavaScript", "React", "Node.js"],
-            remuneration: "100k-120k",
-            dates: "01/01/2024 - 31/12/2024",
-            nombreCandidatures: 50
-        },
-        {
-            id: 2,
-            entreprise: "Microsoft",
-            titre: "Data Scientist",
-            description: "Analyze and interpret complex data.",
-            competences: ["Python", "Machine Learning", "SQL"],
-            remuneration: "90k-110k",
-            dates: "01/02/2024 - 31/12/2024",
-            nombreCandidatures: 30
-        },
-        {
-            id: 3,
-            entreprise: "Google",
-            titre: "Software Engineer",
-            description: "Develop and maintain web applications.",
-            competences: ["JavaScript", "React", "Node.js"],
-            remuneration: "100k-120k",
-            dates: "01/01/2024 - 31/12/2024",
-            nombreCandidatures: 50
-        },
-        {
-            id: 4,
-            entreprise: "Microsoft",
-            titre: "Data Scientist",
-            description: "Analyze and interpret complex data.",
-            competences: ["Python", "Machine Learning", "SQL"],
-            remuneration: "90k-110k",
-            dates: "01/02/2024 - 31/12/2024",
-            nombreCandidatures: 30
-        },
-        {
-            id: 5,
-            entreprise: "Google",
-            titre: "Software Engineer",
-            description: "Develop and maintain web applications.",
-            competences: ["JavaScript", "React", "Node.js"],
-            remuneration: "100k-120k",
-            dates: "01/01/2024 - 31/12/2024",
-            nombreCandidatures: 50
-        },
-        {
-            id: 6,
-            entreprise: "Microsoft",
-            titre: "Data Scientist",
-            description: "Analyze and interpret complex data.",
-            competences: ["Python", "Machine Learning", "SQL"],
-            remuneration: "90k-110k",
-            dates: "01/02/2024 - 31/12/2024",
-            nombreCandidatures: 30
-        },
-        {
-            id: 7,
-            entreprise: "Google",
-            titre: "Software Engineer",
-            description: "Develop and maintain web applications.",
-            competences: ["JavaScript", "React", "Node.js"],
-            remuneration: "100k-120k",
-            dates: "01/01/2024 - 31/12/2024",
-            nombreCandidatures: 50
-        },
-        {
-            id: 8,
-            entreprise: "Microsoft",
-            titre: "Data Scientist",
-            description: "Analyze and interpret complex data.",
-            competences: ["Python", "Machine Learning", "SQL"],
-            remuneration: "90k-110k",
-            dates: "01/02/2024 - 31/12/2024",
-            nombreCandidatures: 30
-        },
-        {
-            id: 9,
-            entreprise: "Google",
-            titre: "Software Engineer",
-            description: "Develop and maintain web applications.",
-            competences: ["JavaScript", "React", "Node.js"],
-            remuneration: "100k-120k",
-            dates: "01/01/2024 - 31/12/2024",
-            nombreCandidatures: 50
-        },
-        {
-            id: 10,
-            entreprise: "Microsoft",
-            titre: "Data Scientist",
-            description: "Analyze and interpret complex data.",
-            competences: ["Python", "Machine Learning", "SQL"],
-            remuneration: "90k-110k",
-            dates: "01/02/2024 - 31/12/2024",
-            nombreCandidatures: 30
-        },
-        {
-            id: 11,
-            entreprise: "Google",
-            titre: "Software Engineer",
-            description: "Develop and maintain web applications.",
-            competences: ["JavaScript", "React", "Node.js"],
-            remuneration: "100k-120k",
-            dates: "01/01/2024 - 31/12/2024",
-            nombreCandidatures: 50
-        },
-        {
-            id: 12,
-            entreprise: "Microsoft",
-            titre: "Data Scientist",
-            description: "Analyze and interpret complex data.",
-            competences: ["Python", "Machine Learning", "SQL"],
-            remuneration: "90k-110k",
-            dates: "01/02/2024 - 31/12/2024",
-            nombreCandidatures: 30
-        }
-    ];
-
+    const [offres, setOffres] = useState([]);
+    const [error, setError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 12; // Nombre d'éléments par page
     const router = useRouter();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch('http://localhost:8000/index.php?route=offers_display');
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                const data = await res.json();
+                const offresWithCompetencesArray = data.map(offre => ({
+                    ...offre,
+                    competences: typeof offre.competences === 'string' ? offre.competences.split(', ') : []
+                }));
+                setOffres(offresWithCompetencesArray);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    // Pagination logic
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = offres.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(offres.length / itemsPerPage);
+
+    const paginate = (pageNumber) => {
+        if (pageNumber > 0 && pageNumber <= totalPages) {
+            setCurrentPage(pageNumber);
+            window.scrollTo(0, 0); // Remettre la position de défilement en haut de la page
+        }
+    };
 
     return (
         <div>
@@ -148,24 +65,50 @@ export default function Page() {
 
             <div className="container-cards-container">
                 <div className="offer-container">
-                    {offres.map((offre, index) => (
-                        <div key={index} className="offer-card">
-                            <div className="titre">{offre.titre}</div>
-                            <div className="entreprise">{offre.entreprise}</div>
-                            <div className="competences">
-                                {offre.competences.map((competence, i) => (
-                                    <span key={i} className="competence-pill">{competence}</span>
-                                ))}
-                            </div>
-                            <button 
-                                className="apply-button" 
-                                onClick={() => router.push('/fakeOffer')}
-                            >
-                                Candidater
-                            </button>
-                        </div>
-                    ))}
+                    {error ? (
+                        <p>Erreur: {error}</p>
+                    ) : (
+                        currentItems.length > 0 ? (
+                            currentItems.map((offre, index) => (
+                                <div key={index} className="offer-card">
+                                    <div className="titre">{offre.titre_offre}</div>
+                                    <div className="entreprise">{offre.entreprise}</div>
+                                    <div className="competences">
+                                        {offre.competences.length > 0 ? (
+                                            offre.competences.map((competence, i) => (
+                                                <span key={i} className="competence-pill">{competence}</span>
+                                            ))
+                                        ) : (
+                                            <span className="competence-pill">Aucune compétence spécifiée</span>
+                                        )}
+                                    </div>
+                                    <button 
+                                        className="apply-button" 
+                                        onClick={() => router.push('/fakeOffer')}
+                                    >
+                                        Candidater
+                                    </button>
+                                </div>
+                            ))
+                        ) : (
+                            <p>Aucune offre trouvée</p>
+                        )
+                    )}
                 </div>
+            </div>
+
+            <div className="pagination">
+                <button onClick={() => paginate(1)} disabled={currentPage === 1}>&laquo;</button>
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                        key={index + 1}
+                        onClick={() => paginate(index + 1)}
+                        className={currentPage === index + 1 ? 'active' : ''}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+                <button onClick={() => paginate(totalPages)} disabled={currentPage === totalPages}>&raquo;</button>
             </div>
 
             <img className="assets" src="/Assets/separateur-w2b.png"/>
