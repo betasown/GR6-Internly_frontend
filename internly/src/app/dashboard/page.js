aa55f1e31a337687f746f2ed9de841a989491b27
betasown <louis.drouhin@gmail.com>
2025-03-25 16:26:00 +0100

    feat: add dashboard page with title and separator image

---
+++ internly/src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <div>
+            <div className="slide-entreprises-container">
+                <div className="title-container">
+                    <h1 className="title">dashboard</h1>
+                </div>
+            </div>
+            <img className="assets" src="/Assets/separateur-w2b.png"/>
+        </div>
+    );
+}
