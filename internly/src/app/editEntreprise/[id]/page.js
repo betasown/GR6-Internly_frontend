0fe227016421ecfc2240194ae4315c2fbf0cd082
betasown <louis.drouhin@gmail.com>
2025-04-02 10:19:15 +0200

    feat : ajout de vérification des droits d'accès pour l'édition d'entreprise

--- internly/src/app/editEntreprise/[id]/page.js
+++ internly/src/app/editEntreprise/[id]/page.js
@@ -18,6 +18,19 @@ const EditEntreprise = () => {
 
   const [initialData, setInitialData] = useState(null); // Stocke les données initiales
 
+  // Vérification des droits d'accès
+  useEffect(() => {
+    const userCookie = document.cookie.split("; ").find((row) => row.startsWith("user="));
+    if (userCookie) {
+      const user = JSON.parse(decodeURIComponent(userCookie.split("=")[1]));
+      if (user.status !== "admin" && user.status !== "pilote") {
+        router.push("/403/"); // Redirige si l'utilisateur n'est ni admin ni pilote
+      }
+    } else {
+      router.push("/403/"); // Redirige si l'utilisateur n'est pas connecté
+    }
+  }, [router]);
+
   useEffect(() => {
     const fetchEntreprise = async () => {
       try {
